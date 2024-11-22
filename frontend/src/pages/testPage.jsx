3dafa7278bc9e3c7ad61a2569f613bcb57c5dea8
Sascha Holmes <saschaholmes@outlook.com>
2024-11-22 02:26:54 +0000

    Add route template to app and set up layout page

---
+++ frontend/src/pages/testPage.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import { Text } from "@chakra-ui/react";
+function Test(){
+
+
+    return (
+        <>
+        <Header size="6xl" bg="teal.500" color="gray.900" text="Hello World"/>
+        <Text textAlign={"center"} fontSize={20}>Hello There. How nice to see you</Text>
+    </>
+)
+}
+
+export default Test;
