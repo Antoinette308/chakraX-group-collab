2c02c8346d57f0f2421044325e46d51da6b456e8
Charlotte Gale <CharlotteMGale@outlook.com>
2024-11-22 19:20:47 +0000

    setup backend with npm init -y, installations, database.js, .env.example, server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
 //This is the entry point for the server
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+/*===============================================================
+                        REQUIRE ROUTES
+        const featureRoute = require('./routes/featureRoute');
+===============================================================*/
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+/* ===============================================================
+                        USE ROUTES
+        app.use('/api/feature'. featureRoute);
+===============================================================*/
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server successfully running on port ${PORT}`)
+});
