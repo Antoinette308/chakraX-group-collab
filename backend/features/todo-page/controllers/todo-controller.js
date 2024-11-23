c6506e0f1802d114e0eeefc1b55a42033cbb46b8
Charlotte Gale <CharlotteMGale@outlook.com>
2024-11-23 14:44:02 +0000

    pseudocode controller & routes for todo API, test server connection and amend body-parser import etc

---
+++ backend/features/todo-page/controllers/todo-controller.js
@@ -0,0 +1,14 @@
+console.log("CG Start");
+
+// import db connection
+import connection from "../../../config/database";
+
+// Create new todo
+
+// Read all todos
+
+// Update a todo by ID
+
+// Delete a todo by ID
+
+console.log("CG finish");
---
+++ backend/features/todo-page/routes/todo-routes.js
@@ -0,0 +1,14 @@
+// imports
+
+
+// declare router
+
+// Create new todo
+
+// Read all todos
+
+// Update a todo by ID
+
+// Delete a todo by ID
+
+// export router
