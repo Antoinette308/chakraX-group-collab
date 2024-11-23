319944ab316f1f1774852f2a2777668a7811c967
Charlotte Gale <CharlotteMGale@outlook.com>
2024-11-23 20:17:38 +0000

    troubleshoot test environ w/ ES6 syntax, had issues, reverted to taught methods of require() and amended test to handle database connection staying open

---
+++ backend/tests/todo.test.js
@@ -0,0 +1,46 @@
+/* ==================================================================
+                    ES6 SYNTAX IMPORTS
+================================================================== */
+// import request from 'supertest';
+// import express from 'express';
+// import todoRoute from '../features/todo-page/routes/todo-routes.js';
+
+// const app = express();
+// app.use('/todo', todoRoute);
+
+// describe('GET /todo', () => {
+//     it('should return the welcome message', async () => {
+//         const response = await request(app).get('/todo');
+//         expect(response.status).toBe(200);
+//         expect(response.text).toBe("What's on the agenda?")
+//     })
+// })
+
+const request = require('supertest');
+const express = require('express');
+const todoRoute = require('../features/todo-page/routes/todo-routes.js');
+const connection = require('../config/database.js')
+
+const app = express();
+app.use('/todo', todoRoute);
+
+beforeAll((done) => {
+    connection.connect(done);
+});
+
+afterAll((done) => {
+    connection.end((err) => {
+        if (err) {
+            console.error('Error terminating the database connection:', err);
+        }
+        done();
+    });
+});
+
+describe('GET /todo', () => {
+    it('should return the welcome message', async () => {
+        const response = await request(app).get('/todo');
+        expect(response.status).toBe(200);
+        expect(response.text).toBe("What's on the agenda?")
+    });
+});
