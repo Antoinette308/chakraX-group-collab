217d8a1892906ab572fb336ba007283a4f07db6c
Charlotte Gale <CharlotteMGale@outlook.com>
2024-11-25 19:02:55 +0000

    split app imports etc from server.js and place in new file app.js

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+/* ==================================================================
+                    ES6 SYNTAX IMPORTS
+================================================================== */
+import express from 'express';
+import pkg from 'body-parser';
+import cors from 'cors';
+import todoRoute from './features/todo-page/routes/todo-routes.js'
+/*===============================================================
+                        REQUIRE ROUTES
+        import featureRoute './routes/featureRoute';
+===============================================================*/
+
+const app = express();
+const { json } = pkg;
+
+app.use(json());
+app.use(cors());
+
+/* ===============================================================
+                        USE ROUTES
+        app.use('/feature', featureRoute);
+===============================================================*/
+app.use('/todo', todoRoute)
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,8 @@
 //This is the entry point for the server
-
 /* ==================================================================
                     ES6 SYNTAX IMPORTS
 ================================================================== */
-import express from 'express';
-import pkg from 'body-parser';
-import cors from 'cors';
-import todoRoute from './features/todo-page/routes/todo-routes.js'
-/*===============================================================
-                        REQUIRE ROUTES
-        import featureRoute './routes/featureRoute';
-===============================================================*/
-
-const app = express();
-const { json } = pkg;
-
-app.use(json());
-app.use(cors());
-
-/* ===============================================================
-                        USE ROUTES
-        app.use('/feature', featureRoute);
-===============================================================*/
-app.use('/todo', todoRoute)
+import app from './app.js'
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
