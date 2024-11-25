6a4c732bd9813bc2bd2e2385ed4b923ba9e54a98
Charlotte Gale <CharlotteMGale@outlook.com>
2024-11-25 19:09:30 +0000

    populate todo-controller.js with CRUD controllers

--- backend/features/todo-page/controllers/todo-controller.js
+++ backend/features/todo-page/controllers/todo-controller.js
@@ -1,24 +1,63 @@
-console.log("CG Start");
 
 /* ==================================================================
                     ES6 SYNTAX IMPORTS
 ================================================================== */
-// import db connection
-import connection from "../../../config/database.js";
+
+// imports
+import { createTodo, getTodos, getTodoById, updateTodoById, deleteTodoById } from "../models/todo-model.js";
 
 // test welcome message
 export const welcomeMessage = (req, res) => {
     res.send(`What's on the agenda?`);
 }
 
-// Create new todo
+// create a new todo
+export const createTodoController = (req, res) => {
+    console.log('Request body:', req.body);
+    const todo = req.body;
+    createTodo(todo, (error, results) => {
+        if (error) {
+            console.error('Error creating todo:', error);
+            return res.status(500).json({ error: error.message });
+        }
+        res.status(201).json(results);
+    });
+};
 
-// Read all todos
+// read all todos
+export const getTodosController = (req, res) => {
+    getTodos((error, results) => {
+        if (error) return res.status(500).json({ error: error.message });
+        res.status(200).json(results);
+    });
+};
 
-// Update a todo by ID
+// read todo by ID
+export const getTodoByIdController = (req, res) => {
+    const { id } = req.params;
+    // const readDataById = req.body;
+    getTodoById(id, (error, results) => {
+        if (error) return res.status(500).json({ error: error.message });
+        res.status(200).json(results);
+    });
+};
 
-// Delete a todo by ID
+// update todo by ID
+export const updateTodoController = (req, res) => {
+    const { id } = req.params;
+    const updateData = req.body;
+    updateTodoById(id, updateData, (error, results) => {
+        if (error) return res.status(500).json({ error: error.message });
+        res.status(200).json(results);
+    });
+};
 
-export default { welcomeMessage };
-
-console.log("CG finish");
+// delete todo by ID
+export const deleteTodoByIdController = (req, res) => {
+    const { id } = req.params;
+    // const deleteData = req.body;
+    deleteTodoById(id, (error, results) => {
+        if (error) return res.status(500).json({ error: error.message });
+        res.status(200).json(results);
+    })
+};
