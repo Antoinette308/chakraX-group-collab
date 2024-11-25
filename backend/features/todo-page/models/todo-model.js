677d47cc63fa56053b8ed40c71910ad0dacb0828
Charlotte Gale <CharlotteMGale@outlook.com>
2024-11-25 19:08:22 +0000

    populate todo-model.js with CRUD models

--- backend/features/todo-page/models/todo-model.js
+++ backend/features/todo-page/models/todo-model.js
@@ -1,11 +1,52 @@
-// imports
+/* ==================================================================
+                    ES6 SYNTAX IMPORTS
+================================================================== */
+// import db connection
+import connection from "../../../config/database.js";
 
-// create a new todo
+// use todo/feature name declared in routes || const featureName = req.body || and callback instead of req res because it allows for the implementation of mock data etc if necessary
 
-// read all todos
+// Create new todo
+export const createTodo = (todo, callback) => {
+    const query = 'INSERT INTO todos (tasks, completed) VALUES (?, ?)';
+    connection.query(query, [todo.tasks, todo.completed], (error, results) => {
+        if (error) return callback(error);
+        callback(null, { id: results.insertId, ...todo });
+    });
+};
 
-// read todo by ID
+// Read all todos
+export const getTodos = (callback) => {
+    const query = 'SELECT * FROM todos';
+    connection.query(query, (error, results) => {
+        if (error) return callback(error);
+        callback(null, results);
+    });
+};
 
-// update todo by ID
+// Read todo by ID
+export const getTodoById = (id, callback) => {
+    const query = 'SELECT * FROM todos WHERE id = ?';
+    connection.query(query, [id], (error, results) => {
+        if (error) return callback(error);
+        callback(null, results[0]);
+    });
+};
 
-// delete todo by ID
+// Update a todo by ID
+export const updateTodoById = (id, updatedTodo, callback) => {
+    const query = 'UPDATE todos SET tasks = ?, completed = ? WHERE id = ?';
+    connection.query(query, [updatedTodo.tasks, updatedTodo.completed, id], (error, results) => {
+        if (error) return callback(error);
+        callback(null, results)
+    });
+};
+
+// Delete a todo by ID
+export const deleteTodoById = (id, callback) => {
+    const query = 'DELETE FROM todos WHERE id = ?';
+    connection.query(query, [id], (error, results) => {
+        if (error) return callback(error);
+        callback(null, results);
+    });
+};
