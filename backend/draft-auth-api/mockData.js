810b1213752e70a07973db8d05a8d03dee2baee2
CharleyG90 <charleyg.90@gmail.com>
2024-11-26 17:25:52 +0000

    set up accounts-backend, and add draft auth api

---
+++ backend/draft-auth-api/mockData.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        id: 1,
+        username: 'mockuser1',
+        password: 'securepassword1',
+        email: 'mockuser1@example.com',
+        created_at: '2024-11-20'
+    },
+    {
+        id: 2,
+        username: 'mockuser2',
+        password: 'securepassword2',
+        email: 'mockuser2@example.com',
+        created_at: '2024-11-21'
+    }
+]
