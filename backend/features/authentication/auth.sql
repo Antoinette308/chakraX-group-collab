203cfd67338edc29bd196cea5eed417bf54c8aae
CharleyG90 <charleyg.90@gmail.com>
2024-11-28 20:09:07 +0000

    write models & sql

---
+++ backend/features/authentication/auth.sql
@@ -0,0 +1,8 @@
+USE exec_function_db;
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ backend/features/authentication/models/auth-model.js
@@ -0,0 +1,30 @@
+/* ==================================================================
+                    ES6 SYNTAX IMPORTS
+================================================================== */
+// import db connection
+import connection from "../../../config/database.js";
+
+export const createUser = (user, callback) => {
+    const query = 'INSERT INTO users (email, password) VALUES ( ?, ?)';
+    connection.query(query, [user.email, user.password], (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            return callback(error)
+        };
+        const formattedUser = {
+            id: results.insertId,
+            ...user
+        };
+        console.log('User created:', formattedUser);
+        callback(null, formattedUser);
+    });
+};
+
+export const findUserByEmail = (email, callback) => {
+    const query = 'SELECT * FROM users WHERE email = ?';
+    connection.query(query, [email], (error, results) => {
+        if (error) return callback(error);
+        const user = results[0];
+        callback(null, user);
+    });
+};
