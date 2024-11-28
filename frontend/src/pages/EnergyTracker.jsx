e2395496550d7ce3d84902878997efe37e4d9c27
Sascha Holmes <saschaholmes@outlook.com>
2024-11-28 16:53:27 +0000

    Add Instructions component for energy tracker

--- frontend/src/pages/EnergyTracker.jsx
+++ frontend/src/pages/EnergyTracker.jsx
@@ -3,6 +3,7 @@ import ActivityButton from "../components/energy-tracker/ActivityButton";
 import Header from "../components/Header";
 import { useState, useEffect } from "react";
 import DailyEnergy from "../components/energy-tracker/DailyEnergy";
+import Instructions from "../components/energy-tracker/Instructions";
 
 function EnergyTracker() {
     const [activities, setActivities] = useState([]);
@@ -29,7 +30,11 @@ function EnergyTracker() {
             { id: 1, activity: "Get Out Of Bed", spoons: 1},
             { id: 2, activity: "brush teeth", spoons: 2 },
             { id: 3, activity: "Stuff", spoons: 1 },
-            {id: 4, activity: "Sleep", spoons: 1}
+            {id: 4, activity: "Sleep", spoons: 1},
+            {id: 5, activity: "Sleep", spoons: 1},
+            {id: 6, activity: "Sleep", spoons: 1},
+            {id: 7, activity: "Sleep", spoons: 1},
+            {id: 8, activity: "Sleep", spoons: 1}
         ];
         setActivities(mockActivities);
     }, []);
@@ -37,9 +42,12 @@ function EnergyTracker() {
     return (
         <Box>
             <Header size="6xl" bg="teal.500" color="gray.900" text="Energy Tracker"/>
-                <DailyEnergy isMobile={isMobile} spoons={spoons} onValueChange={(e) => setSpoons(e.value)} />
-                <Flex justifyContent={"center"} gap="10px" >
-                    <Flex width="75%" justifyContent={"space-evenly"} marginTop="10px">
+                <Flex justifyContent={"center"} gap="10px" flexDir={"column"} alignItems={"center"}>
+                    <Flex justifyContent={"space-evenly"} width="75%" marginTop="10px" gap={"10px"}>
+                    <DailyEnergy isMobile={isMobile} spoons={spoons} onValueChange={(e) => setSpoons(e.value)} />
+                    <Instructions isMobile={isMobile} />
+                    </Flex>
+                    <Flex width="75%" justifyContent={"space-evenly"} marginTop="10px" flexWrap={"wrap"} gap="10px">
                     {activities.map((a) => {
                         return <ActivityButton key={a.id} text={a.activity} default={a.spoons} />
                         })}
