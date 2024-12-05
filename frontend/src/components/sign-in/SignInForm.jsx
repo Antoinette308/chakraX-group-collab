1e42a0c069d4189c3f029869a34c0be3c6a4c908
Antoinette Titre <antoinettetitre@hotmail.com>
2024-12-05 19:20:00 +0000

    Create components for the sign in page

--- frontend/src/components/sign-in/SignInForm.jsx
+++ frontend/src/components/sign-in/SignInForm.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import styles from '../../styles/SignIn.module.css';
 
 
 function SignInForm() {
     return (
-        <div>
+        <>
             <form>
-                <Button />
+                <label className={styles.label}>Username</label>
+                <input type="text" placeholder="Enter your username..." className={styles.input} />
+                <label className={styles.label}>Password</label>
+                <input type="password" placeholder="Enter your password..." className={styles.input} />
+                <button className={styles.forgotPassword}>Forgot Password?</button>
+                <button className={styles.login}>LOGIN</button>
+                <button className={styles.orSignUpUsing}>Or Sign Up Using</button>
+                <button className={styles.singUp}>SIGN UP</button>
             </form>
-        </div>
-    )
+        </>
+    );
 }
 
 export default SignInForm;
