c9ef71ea8a64bbf19f6b13c039e80a5d774aa796
Antoinette Titre <antoinettetitre@hotmail.com>
2024-12-06 01:00:35 +0000

    Create all components for SignIn Page. Create states for the inputs. Create functions to handle form submissions and a route to the SignUp page

--- frontend/src/components/sign-in/SignInForm.jsx
+++ frontend/src/components/sign-in/SignInForm.jsx
@@ -1,20 +1,92 @@
 import React, { useState } from 'react';
 import styles from '../../styles/SignIn.module.css';
+import { useNavigate } from 'react-router-dom';
 
 
 function SignInForm() {
+
+    // States for sign in
+    const [username, setUsername] = useState('');  // Would username be a combination of the first and last name from SignUpForm.jsx?
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');  // Use the email input from the SignUpForm page?
+
+    // Set states for checking errors with the form submission
+    const [submitted, setSubmitted] = useState(false); // If the form is submitted then set to true
+    const [error, setError] = useState(false); // If there is an error then set to true
+    const navigate = useNavigate();  // Use the navigate function from react-router-dom to navigate to the sign-up page using the onClick event
+
+
+    // A function to fetch the user data through API call could go here
+
+
+    // Function to handle email
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        setSubmitted(false); // If the user is typing then set to false
+    };
+
+    // Function to handle password 
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        setSubmitted(false); // If the user is typing then set to false
+    };
+
+
+    // Funtion to handle the form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (email === '' || password === '') {
+            setError(true);
+        } else {
+            setSubmitted(true);
+            setError(false);
+
+
+            // The sign-in functionallity will be added here
+        }
+    };
+
+
+    // Successfully signed-in message
+    function successMessage() {
+        return (
+            <div 
+                className="success"
+                style={{ display: submitted ? "" : "none" }}
+            >
+                <h1>Welcome back, {email}!</h1>
+            </div>
+        );
+    };
+
+
+    // Error message if error is true
+    function errorMessage() {
+        return (
+            <div 
+                className='error'
+                style={{ display: error ? '' : "none" }}
+                >
+                    <h1>Please enter all the fields</h1>
+            </div>
+        );
+    };
+
+        
+
     return (
         <>
-            <form>
-                <label className={styles.label}>Username</label>
-                <input type="text" placeholder="Enter your username..." className={styles.input} />
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <label className={styles.label}>Email</label>
+                <input type="email" placeholder="Enter your email..." className={styles.input} onChange={handleEmail} value={email} />
                 <label className={styles.label}>Password</label>
-                <input type="password" placeholder="Enter your password..." className={styles.input} />
-                <button className={styles.forgotPassword}>Forgot Password?</button>
-                <button className={styles.login}>LOGIN</button>
-                <button className={styles.orSignUpUsing}>Or Sign Up Using</button>
-                <button className={styles.singUp}>SIGN UP</button>
+                <input type="password" placeholder="Enter your password..." className={styles.input} onChange={handlePassword} value={password} />
+                <button type="button" className={styles.forgotPassword}>Forgot Password?</button>
+                <button type="submit" className={styles.login}>LOGIN</button>  {/* Use the onClick event to send data to backend for auth and navigate to the home page? */}
+                <button type="button" className={styles.orSignUpUsing} onClick={() => navigate('/sign-up')}>Or Sign Up Using</button>  {/* This button will navigate to the sign-up page */}
+                <button type="button" className={styles.singUp} onClick={() => navigate('/sign-up')}>SIGN UP</button>  {/* This button will navigate also navigate to the sign-up page */}
             </form>
+            <div className='messages'>{errorMessage()}{successMessage()}</div>
         </>
     );
 }
