88ed65ea285d144dd1a2d962f9ca28024e91da9e
Eve Moore <eveiaim98@outlook.com>
2024-12-06 15:13:09 +0000

    Initial layout for rewards tables

--- backend/features/rewardsSystem-page/rewardsSystem_table.sql
+++ backend/features/rewardsSystem-page/rewardsSystem_table.sql
@@ -1,8 +1,20 @@
 USE exec_function_db;
 
-CREATE table users_rewards (
+CREATE TABLE user_rewards (
     user_id INT NOT NULL,
+    reward_id INT PRIMARY KEY AUTO_INCREMENT,
+    reward VARCHAR(255) NOT NULL,
+    forks_cost INT NOT NULL,
+    streak INT,
+    total_forks INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
-    FOREIGN KEY (user_id) REFERENCES 
-)
+CREATE TABLE daily_login (
+    user_id INT NOT NULL,
+    date DATE PRIMARY KEY,
+    logged_in BOOLEAN,
+    last_visited DATE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
