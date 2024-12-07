39f1877ad96f7c27450df1f2f7d5ed3f8e15cb73
Eve Moore <eveiaim98@outlook.com>
2024-12-07 10:20:45 +0000

    Edited tables, created two models

--- backend/features/rewardsSystem-page/models/rewardsSystem-model.js
+++ backend/features/rewardsSystem-page/models/rewardsSystem-model.js
@@ -6,9 +6,37 @@ dotenv.config();
 // handle user rewards
 /* 
 this will need:
-First model to have a select all using user id as a WHERE clause
-Second model to insert data into the table
-*/
+First model to have a select all using user id as a WHERE clause*/
+export const getAllRewards = (id, callback) => {
+    const query = 'SELECT * FROM user_rewards WHERE id = ?';
+    connection.query(query, [id], (error, results) => {
+        if (error) return callback(error);
+        callback(null, results[0]);
+    });
+};
+/*Second model to insert data into the table*/
+export const createReward = (reward, callback) => {
+    const query = 'INSERT INTO user_rewards (forks, reward_name, created_at) VALUES (?, ?, ?)';
+
+    const values = [
+        reward.forks,
+        reward.reward_name,
+        reward.created_at
+    ];
+
+    connection.query(query, values, (error, results) => {
+        if (error) return callback(error);
+        const formattedReward = {
+            user_id: results.insertId,
+            rewards_id: results.insertId,
+
+            ...reward,
+            created_at: format(new Date(reward.current_date), 'YYYY-MM-DD')
+        };
+        callback(null, formattedReward);
+    });
+};
+
 
 // handle daily login
 /*
--- backend/features/rewardsSystem-page/rewardsSystem_table.sql
+++ backend/features/rewardsSystem-page/rewardsSystem_table.sql
@@ -1,28 +1,6 @@
 USE exec_function_db;
 
 CREATE TABLE user_rewards (
-    user_id INT NOT NULL,
-    reward_id INT PRIMARY KEY AUTO_INCREMENT,
-    reward VARCHAR(255) NOT NULL,
-    forks_cost INT NOT NULL,
-    streak INT,
-    total_forks INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-
-CREATE TABLE daily_login (
-    user_id INT NOT NULL,
-    date DATE PRIMARY KEY,
-    logged_in BOOLEAN,
-    last_visited DATE,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-
-/* My thoughts on this:
-
--- personal rewards table
-
-CREATE TABLE user_rewards (
     reward_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     forks INT NOT NULL,
@@ -41,5 +19,3 @@ CREATE TABLE daily_login (
     weeks INT DEFAULT 1,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
-*/
